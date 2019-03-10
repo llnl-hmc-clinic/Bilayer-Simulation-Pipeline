5abff0153bfefb3eb57360e55471d119c0c1c8b7
mli-cs-hmc <mli18@cmc.edu>
2019-03-10 16:40:11 -0700

    new setups

--- mkcsv.py
+++ mkcsv.py
@@ -4,17 +4,24 @@ import numpy as np
 CONFIGURATIONS = "./configurations.csv"#this is the output file
 
 simulations = {'simulation1': 
-				{'lipid type': ['DOPC', 'POPC', 'PAPE'],
-				'upperLeaflet': ['300', '300', '400'],
-				'lowerLeaflet': ['300', '300', '400'],
-				'asymmetry': ['0', '25', '50', '75'],
-				'steps': '15000000'},
-				'simulation2': 
-				{'lipid type': ['DPPC', 'POPC', 'PAPE'],
-				'upperLeaflet': ['200', '300', '500'],
-				'lowerLeaflet': ['200', '300', '500'],
-				'asymmetry': ['0', '25', '50', '75', '100'],
-				'steps': '15000000'}
+				{'lipid type': ["POPC", "PAPC", "POPE", "DIPE", "DPSM", "PAPS", "PAP6", "CHOL"],
+				'upperLeaflet': [243, 121, 20,  61, 242,   0,  0, 313],
+				'lowerLeaflet': [139,  75, 54, 161, 108, 161, 22, 280],
+				'asymmetry': [13, 23, 24],
+				'sim0':['steep', '0.01', '10000', '2'],
+				'sim1':['md', '0.001', '300000', '4'],
+				'sim2':['md', '0.005', '300000', '6'],
+				'sim3':['md', '0.015', '500000', '8']},
+		'simulation2': 
+				{'lipid type': ["POPC", "PAPC", "POPE", "DIPE", "DPSM", "PAPS", "PAP6", "CHOL"],
+				'lowerLeaflet': [243, 121, 20,  61, 242,   0,  0, 313],
+				'upperLeaflet': [139,  75, 54, 161, 108, 161, 22, 280],
+				'asymmetry': [13, 23, 24],
+				'sim0':['steep', '0.01', '10000', '2'],
+				'sim1':['md', '0.001', '300000', '4'],
+				'sim2':['md', '0.005', '300000', '6'],
+				'sim3':['md', '0.015', '500000', '8']},
+		'end':{}
 }
 
 
@@ -22,20 +29,10 @@ simulations = {'simulation1':
 def main():
 	with open(CONFIGURATIONS, 'w') as f:
 		for key, value in simulations.items():
-			f.write(key + '\n')
-			f.write('lipid type,')
-			for lipid in value['lipid type']:
-				f.write(lipid + ",")
-			f.write('\nupperLeaflet,')
-			for up in value['upperLeaflet']:
-				f.write(str(up) + ",")
-			f.write('\nlowerLeaflet,')
-			for low in value['lowerLeaflet']:
-				f.write(str(low) + ",")
-			f.write('\nasymmetry,')
-			for asym in value['asymmetry']:
-				f.write(str(asym) + ",")
-			f.write('\nsteps,')
-			f.write(value['steps'] + ",")
-			f.write('\n')
+			f.write(str(key) + '\n')
+			for key, value in value.items():
+				f.write(str(key) + ',')
+				for i in value:
+					f.write(str(i) + ",")
+				f.write('\n')
 main()
